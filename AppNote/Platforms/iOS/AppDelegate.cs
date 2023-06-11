b615be8abf59a3baaed0d3c0e96a50c27f742940
fadelalimosa <88129985+fadelalimosa@users.noreply.github.com>
2023-06-11 19:52:11 +0300

    Delete AppNote directory

--- AppNote/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace AppNote;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
